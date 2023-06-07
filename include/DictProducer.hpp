b076a302f3e9c10ef7ee1dbbb81e5b5fd826cdab
robot_nan <2073322374@qq.com>
2023-06-07 23:07:24 +0800

    空

---
+++ include/DictProducer.hpp
@@ -0,0 +1,29 @@
+#ifndef __DICTPRODUCER_HPP
+#define __DICTPRODUCER_HPP
+
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+using std::vector;
+using std::string;
+using std::map;
+using std::set;
+
+class DictProducer
+{
+public:
+    DictProducer(const string &dir);//英文语料目录 
+    ~DictProducer(); 
+    void buildEnDictAndIndex();             //创建英文字典和英文索引
+    void storeEnDict();             //存储英文词典
+    void preProcessLine(string &line);
+
+
+private:
+    vector<string> _files;
+    map<string,int> _dict;
+    map<string,set<int>> _index;
+
+};
+#endif
